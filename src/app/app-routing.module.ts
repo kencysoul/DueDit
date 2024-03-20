a5915ec8d3a402e1a917cf2c4542b50da7879431
kencysoul <johnkennethgalapia@gmail.com>
2024-03-20 14:03:45 +0800

    group act 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AnotherPage } from './another/another.page';
+import { AuthorizationService } from './authorization.service';
+import { SharedPage } from './shared/shared.component';
+import { thirdPage } from './third page/third.page';
 
 const routes: Routes = [
   {
@@ -7,14 +11,28 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'assignment1',
-    loadChildren: () => import('./assignment1/assignment1.module').then( m => m.Assignment1Module)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'my-custom-page',
+    loadChildren: () => import('./my-custom-page/my-custom-page.module').then( m => m.MyCustomPagePageModule)
+  },
+  {
+    path: 'another',
+    component: AnotherPage,
+    canActivate : [AuthorizationService]
+  },
+  {
+    path: 'third page',
+    component: thirdPage,
+    canActivate : [AuthorizationService]
+  },
+  {
+    path: 'shared',
+    component: SharedPage,
+  }
 ];
 
 @NgModule({
