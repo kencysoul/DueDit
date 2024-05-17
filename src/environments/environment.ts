4e495bd98e3e0b7f9b15a30ccc65348bd12b0c20
kencysoul <johnkennethgalapia@gmail.com>
2024-05-17 22:15:10 +0800

    inital commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB80D3yKWEZ9sB7Q2m0lJLsz0pPpvMPm7w",
+    authDomain: "duedit-project.firebaseapp.com",
+    projectId: "duedit-project",
+    storageBucket: "duedit-project.appspot.com",
+    messagingSenderId: "357255820927",
+    appId: "1:357255820927:web:301a999c3c438713b2f14a"
+  }
 };
 
 /*
